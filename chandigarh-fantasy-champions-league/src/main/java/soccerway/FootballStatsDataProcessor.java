73b092d8e4273dd4fdc36ab782ca60e3d196164a
Henry Darana <henry.darana@tray.com>
2025-10-02 17:50:46 +0530

    Soccerway updated again

--- chandigarh-fantasy-champions-league/src/main/java/soccerway/FootballStatsDataProcessor.java
+++ chandigarh-fantasy-champions-league/src/main/java/soccerway/FootballStatsDataProcessor.java
@@ -13,29 +13,41 @@ import static soccerway.Utils.substituteRowInTemplate;
 
 public class FootballStatsDataProcessor {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().clearDriverCache().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(Pathways.UCL_PAGE);
+        //driver.findElement(By.xpath(Pathways.XPATH_CONSENT)).click();
+        Utils.sleep(1000);
 
-        System.out.println("Team Name\t#\tName\tAge\tPosition\tMinutes\tAppearance\tLineups\tSub-In\tSub-Out\tSub-Bench\tGoals\tAssists\tYellow\tSecond Yellow\tRed");
+        System.out.println(
+                "Team Name\t#\tName\tAge\tPosition\tMinutes\tAppearance\tLineups\tSub-In\tSub-Out\tSub-Bench\tGoals\tAssists\tYellow\tSecond Yellow\tRed");
 
         List<WebElement> listOfTeamURLs = driver.findElements(By.xpath(Pathways.XPATH_ALL_UCL_TEAMS_URLS));
         List<String> listOfTeamURL = new ArrayList<>();
-        for (int j = 0; j < 32; j++) {
+        for (int j = 0; j < 36; j++) {
             listOfTeamURL.add(listOfTeamURLs.get(j).getAttribute("href"));
         }
 
-        for (int j = 0; j < 32; j++) {
+//        List<String> listOfTeamURL = new ArrayList<>();
+//        listOfTeamURL.add("https://int.soccerway.com/teams/france/paris-saint-germain-fc/886/");
+//        listOfTeamURL.add("https://int.soccerway.com/teams/italy/fc-internazionale-milano/1244/");
+
+        for (int j = 0; j < 36; j++) {
+            //for (int j = 0; j < 4; j++) {
             driver.get(listOfTeamURL.get(j));
+            Utils.sleep(2000);
             driver.findElement(By.xpath(Pathways.XPATH_SQUAD)).click();
+            Utils.sleep(2000);
+            //driver.findElement(By.xpath(Pathways.XPATH_STATISTICS)).click();
             Utils.sleep(5000);
             int playerCount = driver.findElements(By.xpath(Pathways.XPATH_STATS_TABLE_ROW_SIZE)).size();
+            //System.out.println(playerCount);
             String teamName = driver.findElement(By.xpath(Pathways.XPATH_TEAM_NAME)).getText();
 
-            for (int i = 2; i <= playerCount; i++) {
+            for (int i = 1; i <= playerCount; i++) {
                 String shirtNumber = "";
                 String name = "";
                 String age = "";
@@ -56,21 +68,22 @@ public class FootballStatsDataProcessor {
                     shirtNumber = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SHIRT_NUMBER, i))).getText();
                     name = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_NAME, i))).getText();
                     age = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_AGE, i))).getText();
-                    position = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_POSITION, i))).getText();
+                    //position = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_POSITION, i))).getText();
                     minutes = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_MINUTES, i))).getText();
                     appearance = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_APPEARANCE, i))).getText();
-                    lineups = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_LINEUP, i))).getText();
-                    subIn = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SUB_IN, i))).getText();
-                    subOut = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SUB_OUT, i))).getText();
-                    subOnBench = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SUB_ON_BENCH, i))).getText();
+                    //lineups = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_LINEUP, i))).getText();
+                    //subIn = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SUB_IN, i))).getText();
+                    //subOut = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SUB_OUT, i))).getText();
+                    //subOnBench = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SUB_ON_BENCH, i))).getText();
                     goals = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_GOALS, i))).getText();
                     assists = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_ASSISTS, i))).getText();
                     yellow = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_YELLOW, i))).getText();
-                    secondYellow = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SECOND_YELLOW, i))).getText();
+                    //secondYellow = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_SECOND_YELLOW, i))).getText();
                     red = driver.findElement(By.xpath(substituteRowInTemplate(Pathways.XPATH_TEMPLATE_RED, i))).getText();
-                    } catch (Exception e) {
+                } catch (Exception e) {
                 }
-                System.out.println(teamName + "\t" + shirtNumber + "\t" + name + "\t" + age + "\t" + position + "\t" + minutes + "\t" + appearance + "\t" + lineups + "\t" + subIn + "\t" + subOut + "\t" + subOnBench + "\t" + goals + "\t" + assists + "\t" + yellow + "\t" + secondYellow + "\t" + red);
+                System.out.println(
+                        teamName + "\t" + shirtNumber + "\t" + name + "\t" + age + "\t" + position + "\t" + minutes + "\t" + appearance + "\t" + lineups + "\t" + subIn + "\t" + subOut + "\t" + subOnBench + "\t" + goals + "\t" + assists + "\t" + yellow + "\t" + secondYellow + "\t" + red);
             }
 
         }
